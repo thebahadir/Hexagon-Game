e1e05b86aa02dac2dfe8cd37239ca17ef5bad0a0
thebahadir <akyazibahadir@gmail.com>
2021-01-04 16:22:48 +0300

    yeni dosyalar

---
+++ Assets/Scripts/Hexagon.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hexagon : MonoBehaviour
+{
+    public float shrinkSpeed = 2.5f;
+    private Rigidbody2D RB;
+
+
+    void Start()
+    {
+        // Hexagon RigidBody2D'sine ulaş.
+        RB = GetComponent<Rigidbody2D>();
+
+        // Hexagon spawnerlarını 0 - 360 derece arasında random olarak döndür.
+        RB.rotation = Random.Range(0f, 360f);
+
+        // Spawn boyutları.
+        transform.localScale = Vector3.one * 10f;
+
+    }
+
+    void Update()
+    {
+        // Hexagon spawnerlarının boyutunu küçülttük ve küçülme hızını ekle.
+        transform.localScale -= Vector3.one * Time.deltaTime * shrinkSpeed;
+
+        // Spawnelar 0.1'den küçük olduğu takdirde spawnerı yok et.
+        if (transform.localScale.x < 0.1f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Awake()
+    {
+        // Rastgele renk oluştur.
+        Color newColor = new Color(Random.value, Random.value, Random.value, 1.0f);
+
+        // Oluşturulan rengi hexagon spawnerlarına ait materyale uygula.
+        GetComponent<Renderer>().material.color = newColor;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public float movementSpeed = 500f;
+    private float movement = 0f;
+    private GameManager game;
+    public Transform effect;
+    public RectTransform gameOver;
+    private Spawner spawner;
+
+    [HideInInspector]
+    public bool dead;
+
+
+    private void Awake()
+    {
+        //Game Over ekranını oluşturan panel gizlendi.
+        gameOver.localScale = new Vector3(0, 0);
+    }
+
+    void Start()
+    {
+        game = GameObject.Find("GameManager").GetComponent<GameManager>();
+        spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
+        dead = false;
+    }
+
+    void Update()
+    {
+       
+        if (!dead)
+        {
+            movement = Input.GetAxisRaw("Horizontal");
+        }
+
+        else
+        {
+            if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+            {
+                SceneManager.LoadScene("Game");
+            }
+        }
+
+    }
+
+     void FixedUpdate()
+    {
+        // Player sıfır ekseninde, EnergySphere objesinin çevresinde döndürüldü.
+        transform.RotateAround(Vector3.zero, Vector3.forward, movement * -movementSpeed * Time.fixedDeltaTime);
+    }
+
+     void OnTriggerEnter2D(Collider2D collision)
+    {        
+
+        if (collision.gameObject.CompareTag("Hexagon") && !dead) 
+        {                                                       
+            Death();
+            Invoke(nameof(GameOver), 0.5f);
+            Destroy(spawner);
+            game.scoreText.fontSize = 0;
+        }
+
+        else if (collision.gameObject.CompareTag("Point"))
+        {
+            game.score++;
+            game.ChangeScore();
+        }
+    }
+
+    void Death()
+    {
+        dead = true;
+        game.music.Stop();
+        game.effectsSounds.clip = game.deathSound;
+        game.effectsSounds.Play();
+        game.effectsSounds2.clip = game.gameOverSound;
+        game.effectsSounds2.Play();
+
+        Instantiate(effect, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+
+    void GameOver()
+    {
+        //Game Over ekranını oluşturan panel gösterildi.
+        gameOver.localScale = new Vector3(1, 1);
+    }
+
+}
