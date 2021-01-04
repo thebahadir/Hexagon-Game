b890060966a355cfcae3b3e62ccf626d87868f28
thebahadir <akyazibahadir@gmail.com>
2021-01-05 01:41:34 +0300

    history deleted

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+
+{
+    
+    public GameObject gameObjects;
+    private bool gameStarted;
+    public Text scoreText;
+    public Text scoreText2;
+    public int score;
+    
+
+    public AudioSource music;
+    public AudioSource effectsSounds;
+    public AudioClip deathSound;
+    public AudioSource effectsSounds2;
+    public AudioClip gameOverSound;
+
+
+    void Start()
+    {
+        gameStarted = false;
+        scoreText.text = "0";
+
+    }
+
+    void Update()
+    {
+        if (!gameStarted)
+        {
+                gameStarted = true;
+                gameObjects.SetActive(true);
+                scoreText.text = "0";
+        }
+
+    }
+
+    public void ChangeScore()
+    {
+        scoreText.text = score.ToString();
+        scoreText2.text = "Score\n" + score.ToString();
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public float spawnRate;
+    public GameObject hexagonPrefab;
+    private float nexTimeToSpawn;
+    
+
+    void Update()
+    {
+        
+        if (Time.time >= nexTimeToSpawn)
+        {
+            Instantiate(hexagonPrefab);
+            nexTimeToSpawn = Time.time + spawnRate;
+        }
+    }
+
+}
